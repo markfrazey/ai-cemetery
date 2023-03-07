ebebe17073ffd2dcbb0cbe95d8fe37480d85dabc
bwmendo <bwmendo@uw.edu>
2023-03-06 18:26:54 -0800

    Added about page (just summary)

--- src/About.js
+++ src/About.js
@@ -2,6 +2,9 @@ export default function About() {
   return (
     <>
       <h1>About</h1>
+      <p>Welcome to Chatbot Cemetery, a website dedicated to exploring the rise and fall of chatbots over the past six years.</p>
+      <p>As the popularity of chatbots grew, so did the number of bots created. However, not all chatbots were successful and many were shut down due to various reasons.</p>
+      <p>Through our website, we aim to provide insight into why certain chatbots failed, whether it was due to lack of user engagement, technological limitations, or ethical concerns.</p>
     </>
   );
 }
