d46bcf1ca51f3b0239d69bae3e6a93949ba6720c
bwmendo <bwmendo@uw.edu>
2023-03-08 12:20:06 -0800

    Added Meet the Team

--- src/About.js
+++ src/About.js
@@ -2,6 +2,14 @@ import markImage from './img/Mark.jpg';
 import nilaImage from './img/Nila.jpg';
 import minsuhImage from './img/Minsuh.jpg';
 import brandonImage from './img/Brandon.jpeg';
+<<<<<<< HEAD
+=======
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
+import { Typography } from '@mui/material';
+
+>>>>>>> 17f294a (Added Meet the Team)
 
 export default function About() {
 
@@ -20,7 +28,8 @@ export default function About() {
     },
     {
       name: 'Brandon Mendoza',
-      image: brandonImage    }
+      image: brandonImage
+    }
   ];
 
   return (
@@ -29,12 +38,41 @@ export default function About() {
       <p>Welcome to Chatbot Cemetery, a website dedicated to exploring the rise and fall of chatbots over the past six years.</p>
       <p>As the popularity of chatbots grew, so did the number of bots created. However, not all chatbots were successful and many were shut down due to various reasons.</p>
       <p>Through our website, we aim to provide insight into why certain chatbots failed, whether it was due to lack of user engagement, technological limitations, or ethical concerns.</p>
-      {teamMembers.map((member, index) => (
-        <div key={index}>
-          <img src={member.image} alt={member.name} />
-          <h2>{member.name}</h2>
-        </div>
-      ))}
-    </>
+      <h1>Meet The Team</h1>
+      <Grid2 container spacing={3}>
+        <Grid2>
+          <Avatar
+        alt="Mark Frazey"
+        src={markImage}
+        sx={{ width: 200, height: 200 }}
+          />
+        <Typography variant="h5" textAlign="center">Mark Frazey</Typography>
+      </Grid2>
+      <Grid2>
+        <Avatar
+          alt="Brandon Mendoza"
+          src={brandonImage}
+          sx={{ width: 200, height: 200 }}
+        />
+        <Typography variant="h5">Brandon Mendoza</Typography>
+        </Grid2>
+        <Grid2>
+          <Avatar
+            alt="Nila Ragu"
+            src={nilaImage}
+            sx={{ width: 200, height: 200 }}
+          />
+          <Typography variant="h5" textAlign="center">Nila Ragu</Typography>
+      </Grid2>
+      <Grid2>
+        <Avatar
+          alt="Minsuh Kim"
+          src={minsuhImage}
+          sx={{ width: 200, height: 200 }}
+        />
+        <Typography variant="h5" textAlign="center">Minsuh Kim</Typography>
+      </Grid2>
+    </Grid2>
+  </>
   );
 }
