e68fb7f268f17a0885168e7ae8f4c70dccf16d99
Mark <markfrazey@gmail.com>
2023-03-09 11:02:04 -0800

    Added functionality to cemetery and home

--- src/Blog.js
+++ src/Blog.js
@@ -2,26 +2,28 @@ import Fade from '@mui/material/Fade';
 
 export default function Blog() {
   return (
-    <>
-      <h1>Blog</h1>
-      <p>
-        From all that we have done, our intended impact of the project is for people to improve from the past errors in AI to understand and eliminate biased results and prevent any further issues with AI models. However, what we at least expect for the actual impact is to give people a more thorough understanding of why several of the models have failed and become more aware of this ongoing issue. Even with our expectations on the purpose of this project are promising, it will not fully achieve the full intended impact that we hoped for since biased data originally comes from personal opinions and judgments of different people, meaning that handling every single type of bias will be very difficult to analysis one at a time. Either way, it is impossible to get rid of bias entirely, but we believe that this knowledge will make people more aware of the potential issues when working with AI. If all goes well, the status of the project will expand upon this in the future with information on current AIs in progress for users to analyze.
-      </p>
-      <p>
-      For our final deliverable, we constructed a website titled “Chatbot Cemetery”. Its purpose is to detail major chatbots that have been shut down due to various reasons, such as privacy, racism, and safety concerns. The inspiration for the website came from Google Cemetery, which lists all of the Google products and services that have been shut down over time.
-      </p>
-      <p>
-      The Home page of the website features a timeline of ten chosen chatbots that have been shut down. The timeline is designed to resemble a graveyard, with each chatbot represented by a card that includes their name, lifespan, and a short epitaph. The purpose of the Home page is to provide a quick overview of the chatbots that have been shut down and their reasons for being decommissioned.
-      </p>
-      <p>
-      On the Cemetery page, users can see a comprehensive list of all the dead chatbots. The page also offers filtering options based on various attributes, such as the chatbot's creator, the year it was active, or the reason for its shutdown. This page allows users to explore the chatbots in more detail and gain a deeper understanding of the reasons behind their shutdown.
-      </p>
-      <p>
-      Finally, the About page provides context for the project and introduces the team members who worked on it. This page sheds light on the scope of the project and its goals, as well as the people who dedicated their time and effort to make the project a reality. It adds a personal touch to the website and helps users connect with the creators behind the project.
-      </p>
-      <p>
-      Overall, the Chatbot Cemetery website serves as a valuable resource for anyone interested in the history and development of chatbots. It provides insight into the reasons behind their shutdowns and helps users gain a better understanding of the issues that arise when working with AI technology.
-      </p>
+    <Fade in={true}  mountOnEnter unmountOnExit>
+      <>
+        <h1>Blog</h1>
+        <p>
+          From all that we have done, our intended impact of the project is for people to improve from the past errors in AI to understand and eliminate biased results and prevent any further issues with AI models. However, what we at least expect for the actual impact is to give people a more thorough understanding of why several of the models have failed and become more aware of this ongoing issue. Even with our expectations on the purpose of this project are promising, it will not fully achieve the full intended impact that we hoped for since biased data originally comes from personal opinions and judgments of different people, meaning that handling every single type of bias will be very difficult to analysis one at a time. Either way, it is impossible to get rid of bias entirely, but we believe that this knowledge will make people more aware of the potential issues when working with AI. If all goes well, the status of the project will expand upon this in the future with information on current AIs in progress for users to analyze.
+        </p>
+        <p>
+        For our final deliverable, we constructed a website titled “Chatbot Cemetery”. Its purpose is to detail major chatbots that have been shut down due to various reasons, such as privacy, racism, and safety concerns. The inspiration for the website came from Google Cemetery, which lists all of the Google products and services that have been shut down over time.
+        </p>
+        <p>
+        The Home page of the website features a timeline of ten chosen chatbots that have been shut down. The timeline is designed to resemble a graveyard, with each chatbot represented by a card that includes their name, lifespan, and a short epitaph. The purpose of the Home page is to provide a quick overview of the chatbots that have been shut down and their reasons for being decommissioned.
+        </p>
+        <p>
+        On the Cemetery page, users can see a comprehensive list of all the dead chatbots. The page also offers filtering options based on various attributes, such as the chatbot's creator, the year it was active, or the reason for its shutdown. This page allows users to explore the chatbots in more detail and gain a deeper understanding of the reasons behind their shutdown.
+        </p>
+        <p>
+        Finally, the About page provides context for the project and introduces the team members who worked on it. This page sheds light on the scope of the project and its goals, as well as the people who dedicated their time and effort to make the project a reality. It adds a personal touch to the website and helps users connect with the creators behind the project.
+        </p>
+        <p>
+        Overall, the Chatbot Cemetery website serves as a valuable resource for anyone interested in the history and development of chatbots. It provides insight into the reasons behind their shutdowns and helps users gain a better understanding of the issues that arise when working with AI technology.
+        </p>
       </>
+    </Fade>
   );
 }
